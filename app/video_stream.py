8fa94162e5e0226c39dea9d278e6c6e46b73d4c9
massegu <massegu@gmail.com>
2025-11-04 22:54:58 +0000

    modificado por 2 vez video stream para gestionar errores

--- app/video_stream.py
+++ app/video_stream.py
@@ -10,15 +10,19 @@ class FaceMeshTransformer(VideoTransformerBase):
         self.drawing_spec = self.mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1)
 
     def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        results = self.face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        if results.multi_face_landmarks:
-            for face_landmarks in results.multi_face_landmarks:
-                self.mp_drawing.draw_landmarks(
-                    image=img,
-                    landmark_list=face_landmarks,
-                    connections=self.mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=self.drawing_spec,
-                    connection_drawing_spec=self.drawing_spec
-                )
-        return img
+        try:
+            img = frame.to_ndarray(format="bgr24")
+            results = self.face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        image=img,
+                        landmark_list=face_landmarks,
+                        connections=self.mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=self.drawing_spec,
+                        connection_drawing_spec=self.drawing_spec
+                    )
+            return img
+        except Exception as e:
+            print("Error en transform:", e)
+            return frame
