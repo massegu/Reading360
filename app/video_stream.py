5920c2093d04db6bc2bc015c6996281d75d7f9a7
massegu <massegu@gmail.com>
2025-11-05 09:43:19 +0000

    cambiado videoprocessor a recv

--- app/video_stream.py
+++ app/video_stream.py
@@ -1,15 +1,15 @@
 import cv2
 import mediapipe as mp
-from streamlit_webrtc import VideoTransformerBase
+from streamlit_webrtc import VideoProcessorBase
 
-class FaceMeshTransformer(VideoTransformerBase):
+class FaceMeshTransformer(VideoProcessorBase):
     def __init__(self):
         self.mp_face_mesh = mp.solutions.face_mesh
         self.face_mesh = self.mp_face_mesh.FaceMesh(static_image_mode=False)
         self.mp_drawing = mp.solutions.drawing_utils
         self.drawing_spec = self.mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1)
 
-    def transform(self, frame):
+    def recv(self, frame):
         try:
             img = frame.to_ndarray(format="bgr24")
             results = self.face_mesh.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
